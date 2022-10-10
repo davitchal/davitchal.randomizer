ef48e368fcc05c3ec49d81d479994663e5512eea
davitchal <davit.chaloyan@gmail.com>
2022-10-09 17:09:30 -0700

    added randomizer animation

--- sketch.js
+++ sketch.js
@@ -3,41 +3,52 @@ let cars = [{
       model: "miata"
     }, {
       make: "nissan",
-      model: "350Z"
+      model: "350z"
     }, {
       make: "toyota",
-      mdoel: "corolla"
+      model: "corolla"
     }, {
       make: "honda",
-      mdoel: "civic"
+      model: "civic"
     }, {
       make: "bmw",
-      mdoel: "m2"
+      model: "m2"
     }, {
       make: "subaru",
-      mdoel: "impreza"
+      model: "impreza"
     }];
 
     let randomIndex;
+    let animating = false;
 
 function setup() {
   createCanvas(400, 400);
   background(200);
   textSize(32);
+  text("Click to Randomize", 50, 50);
+
 }
 
 function draw() {
+  if (animating == true){
+    ellipse(random(width), random(height), random(50, 200));
+  }
 }
 
-function mousePressed() {
+function randomizer() {
+  animating = false;
   if (cars[0]) {
     background(200);
     randomIndex = int(random(cars.length));
-    text(cars[randomIndex].make, 50, 50);
+    text(`${cars[randomIndex].make} ${cars[randomIndex].model}`, 50, 50);
     cars.splice(randomIndex, 1);
   } else {
     background(200);
     text("Nothing left!", 50, 50);
   }
+}
 
+function mousePressed() {
+  animating = true;
+  setTimeout(randomizer, 1000);
 }
