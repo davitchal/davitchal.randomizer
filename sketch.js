26394cbf5822ddfcf36f55ca7e1ef45c9850dfeb
davitchal <davit.chaloyan@gmail.com>
2022-10-18 21:55:41 -0700

    added images and button

--- sketch.js
+++ sketch.js
@@ -21,28 +21,39 @@ let cars = [{
     let randomIndex;
     let animating = false;
     let car = [];
+    let imageCounter = 0;
+    let button;
 
 function preload(){
       for (let i=0; i<6; i++){
-        //var img = new Image();
-        //img.src = 'assets/car_0.jpg';
-        //car[i] = img;
-        car[i] = loadImage('assets/screenshot.jpg');
+        car[i] = loadImage(`assets/car_${i}.jpg`);
       }
 
     }
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
   background(200);
   textSize(32);
-  text("Click to Randomize", 50, 50);
+  textAlign(CENTER);
+  text("Click to Randomize", width/2,width/2);
   imageMode(CENTER);
+  frameRate(10);
+
+  button = createButton ("Click to Randomize");
+  button.mousePressed(buttonPressed);
 }
 
 function draw() {
   //console.log(car[0]);
   if (animating == true){
-    image(car[0], 10, 10, 50, 50);
+    clear();
+    image(car[imageCounter], width/2, height/2);
+    if (imageCounter < car.length - 1){
+      imageCounter++;
+    }else{
+      imageCounter=0;
+    }
+
   }
 }
 
@@ -51,15 +62,18 @@ function randomizer() {
   if (cars[0]) {
     background(200);
     randomIndex = int(random(cars.length));
-    text(`${cars[randomIndex].make} ${cars[randomIndex].model}`, 50, 50);
-    cars.splice(randomIndex, 1);
+    text(`${cars[randomIndex].make} ${cars[randomIndex].model}`, width/2, height * .75);
+    image(car[randomIndex], width/2, height/2);
+    //cars.splice(randomIndex, 1);
+    //car.splice(randomIndex, 1);
   } else {
     background(200);
     text("Nothing left!", 50, 50);
   }
 }
 
-function mousePressed() {
+function buttonPressed() {
+  randomIndex = int(random(cars.length));
   animating = true;
-  setTimeout(randomizer, 1000);
+  setTimeout(randomizer, 2000);
 }
